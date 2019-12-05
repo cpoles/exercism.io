57f1b2dcc3545c9146a65a0ad580fcb49f1a9c12
cpoles <carlos.poles@alumni.griffithuni.edu.au>
2019-12-05 20:19:14 +1000

    finished Leap Year

--- exercism.io/leap/leapYear.hs
+++ exercism.io/leap/leapYear.hs
@@ -1,1 +1,11 @@
+module LeapYear where
 
+isDivisible :: Int -> Int -> Bool
+isDivisible x y = x `mod` y == 0
+
+isLeapYear :: Int -> Bool
+isLeapYear year
+  | isDivisible year 400  = True
+  | isDivisible year 100  = False
+  | isDivisible year 4    = True
+  | otherwise             = False
