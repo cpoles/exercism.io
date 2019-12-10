5988cf922df21fa5ee9f4cc3301a7bd650d0ce5c
cpoles <carlos.poles@alumni.griffithuni.edu.au>
2019-12-10 16:35:04 +1000

    updated binary string transmitter to reflect changes from exercises 7 and 8

--- hutton/chapter07/binary_str_trans.hs
+++ hutton/chapter07/binary_str_trans.hs
@@ -20,19 +20,39 @@ int2bin n = n `mod` 2 : int2bin (n `div` 2)
 make8 :: [Bit] -> [Bit]
 make8 bits = take 8 (bits ++ repeat 0)
 
+-- function that counts the number of ones in a 8-bit binary number
+countOnes :: [Bit] -> Int
+countOnes xs = foldr (+) 0 (filter (==1) xs)
+
+-- function that adds the ParityBit to the 8-bit binary number
+addParityBit :: [Bit] -> [Bit]
+addParityBit xs | odd $ countOnes xs  = xs ++ [1]
+                | otherwise = xs ++ [0]
+
 -- TRANSMISSION
 -- function that encodes a string of chars as a list of bits 
 encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . ord)
+encode = concat . map (addParityBit . make8 . int2bin . ord)
 
--- function that decodes a list produced by encode - chops a list of into eight-bit binary numbers
-chop8 :: [Bit] -> [[Bit]]
-chop8 []     = []
-chop8 bits   = take 8 bits : chop8 (drop 8 bits)
+-- function that decodes a list produced by encode - chops a list of into 9-bit binary numbers
+chop9 :: [Bit] -> [[Bit]]
+chop9 []     = []
+chop9 bits   = take 9 bits : chop9 (drop 9 bits)
+
+-- function that checks the parity bit, removes it and returns 8-bit binary number
+checkParityBit :: [Bit] -> [Bit] 
+checkParityBit xs = if odd $ countOnes (init xs)
+                      then if last xs == 1
+                           then init xs 
+                           else error "ParityBit error."
+                      else if last xs == 0
+                           then init xs
+                           else error "ParityBit error."
+
 
 -- function that decodes a list of bits as a string of chars by chopping the list up
 decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+decode = map (chr . bin2int . checkParityBit) . chop9
 
 -- function that simulates the transmission of a string of chars as a list of bits
 transmit :: String -> String
