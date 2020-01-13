c08084c188444e6f20bbd155a43c8728f5621277
cpoles <carlos.poles@alumni.griffithuni.edu.au>
2020-01-13 14:24:53 +1000

    Finished ex.4

--- hutton/chapter08/book_samples.hs
+++ hutton/chapter08/book_samples.hs
@@ -75,3 +75,32 @@ occurs x (Node l y r) | x == y    = True
 flatten :: Tree a -> [a]
 flatten (Leaf x)     = [x]
 flatten (Node l x r) = flatten l ++ [x] ++ flatten r
+
+rmdups :: Ord a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) | x `elem` xs = rmdups xs
+              | otherwise = x : rmdups xs
+
+-- Abstract Machine
+data Expr = Val Int | Add Expr Expr
+
+--value :: Expr -> Int
+-- value (Val n)   = n
+-- value (Add x y) = value x + value y
+type Cont = [Op]
+
+data Op = EVAL Expr | ADD Int
+
+eval :: Expr -> Cont -> Int
+eval (Val n)   c = exec c n
+eval (Add x y) c = eval x (EVAL y : c)
+
+exec :: Cont -> Int ->  Int
+exec []           n = n 
+exec (EVAL y : c) n = eval y (ADD n : c)
+exec (ADD n : c)  m = exec c (n + m)
+
+-- redefining value using eval
+
+value :: Expr -> Int
+value e = eval e []
--- hutton/chapter08/chapter08_exercises.hs
+++ hutton/chapter08/chapter08_exercises.hs
@@ -10,5 +10,36 @@ add (Succ m) n = Succ (add m n)
 
 mult :: Nat -> Nat -> Nat
 mult Zero _        = Zero
-mult (Succ Zero) n = n
 mult m _           = add m (add m Zero)
+
+-- Ex.2 Redefine the function occurs using the function compare
+data Tree a = Leaf a | Node (Tree a) a (Tree a)
+
+occurs :: Ord a => a -> Tree a -> Bool
+occurs x (Leaf y)     = x == y
+occurs x (Node l y r) = case x `compare` y of
+                          EQ -> True
+                          LT -> occurs x l
+                          GT -> occurs x r
+
+-- Ex.3 Define a function balanced :: Tree a -> Bool
+data NewTree a = NewLeaf a | NewNode (NewTree a) (NewTree a) deriving (Show)
+
+treeSize :: NewTree a -> Integer
+treeSize (NewLeaf _)   = 1
+treeSize (NewNode l r) = treeSize l + treeSize r 
+
+balanced :: NewTree a -> Bool
+balanced (NewLeaf _)   = True
+balanced (NewNode l r) = abs (treeSize l - treeSize r) <= 1
+                         && balanced l && balanced r
+
+-- Ex.4 Define a function balance :: [a] -> Tree a
+halves :: [a] -> ([a], [a])
+halves xs = splitAt half xs
+            where half = length xs `div` 2
+
+balance :: [a] -> NewTree a
+balance [x]    = NewLeaf x
+balance xs     = NewNode (balance fstHalf) (balance sndHalf)
+             where (fstHalf, sndHalf) = halves xs      
